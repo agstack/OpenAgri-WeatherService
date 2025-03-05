95b3f6d91c6c087d9379131476a1982138daa11c
Yiorgos Marinellis <mari.giorgos@gmail.com>
2025-03-05 12:25:11 +0200

    Disable noisy logging

--- src/services/base.py
+++ src/services/base.py
@@ -90,7 +90,6 @@ class MicroserviceClient:
     async def get(
         self, endpoint: str, params: Optional[Dict[str, Any]] = None, **kwargs
     ):
-        """Make a GET request."""
         return await self.request("GET", endpoint, params=params, **kwargs)
 
     async def post(
